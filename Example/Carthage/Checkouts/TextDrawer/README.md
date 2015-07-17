a53ee58d56f3824bec4029f291c6858de4243106
rémi  <remirobert33530@gmail.com>
2015-07-17 16:01:20 +0200

    up

---
+++ Example/Carthage/Checkouts/TextDrawer/README.md
@@ -0,0 +1,2 @@
+# TextDrawer
+TextDrawer, is a UIView allows you to add text, with gesture, on UIView, or UIImage
---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Remi Robert on 17/07/15.
+//  Copyright (c) 2015 Remi Robert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
