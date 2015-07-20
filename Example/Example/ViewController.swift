74ac0edbf912a096bc9ef9c7557c7e1ba26441ed
rémi  <remirobert33530@gmail.com>
2015-07-20 11:57:40 +0200

    update public API

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -10,20 +10,15 @@ import UIKit
 import TextDrawer
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var drawTextView: TextDrawer!
+    
+    @IBAction func changeTextColor(sender: AnyObject) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
