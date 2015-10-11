65d16351b4e0f279f36f7bce73106c75e8689f60
Ludvig Odin Hestvik <ludvig.hestvik@gmail.com>
2015-10-11 16:23:46 +0200

    Updated to swift 2.0

--- TextDrawer/TextDrawer/DrawTextView.swift
+++ TextDrawer/TextDrawer/DrawTextView.swift
@@ -45,7 +45,7 @@ public class DrawTextView: UIView {
         }
     }
 
-    required public init(coder aDecoder: NSCoder) {
+    required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
--- TextDrawer/TextDrawer/TextDrawer.swift
+++ TextDrawer/TextDrawer/TextDrawer.swift
@@ -105,7 +105,7 @@ public class TextDrawer: UIView, TextEditViewDelegate {
         setup()
     }
     
-    required public init(coder aDecoder: NSCoder) {
+    required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setup()
     }
